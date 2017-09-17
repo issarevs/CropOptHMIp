999ad4d247fe12fe930d629867fb03c9eef20041
issarevs <issarevs@gmail.com>
2017-09-17 14:02:03 -0700

    Initial commit

---
+++ Bootstrapper.cs
@@ -0,0 +1,32 @@
+﻿using Prism.Modularity;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CropOptHMIp
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            App.Current.MainWindow = (Window)Shell;
+            App.Current.MainWindow.Show();
+        }
+
+        protected override DependencyObject CreateShell()
+        {
+            return Container.TryResolve<Shell>();
+        }
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+            ModuleCatalog moduleCatalog = (ModuleCatalog)this.ModuleCatalog;
+            moduleCatalog.AddModule(typeof(ViewsModule.ViewsModule));
+        }
+    }
+}
